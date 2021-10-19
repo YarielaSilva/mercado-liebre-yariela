af0770ed9a2d03a1bb40b119e6566ad29d2a134e
YarielaSilva <yarielasilva17@gmail.com>
2021-10-18 22:06:44 -0300

    agregando lo del puerto a heroku

--- app.js
+++ app.js
@@ -8,10 +8,15 @@ const publicPath = path.resolve (__dirname, './public');
 app.use (express.static(publicPath));
 
 
-
-app.listen (3000, () => {
+/*
+app.listen (process.env.PORT || 3000, () => {
     console.log ("Servidor corriendo en el puerto 3000")
+});*/
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log('Servidor Corriendo en el puerto 3000')
 });
+
 
 app.get('/', (req, res) =>{
     res.sendFile(path.resolve(__dirname,'./views/home.html'))
